f5ace855c4538ebfe28223411bb677c439c52ef4
John Elstad <391511@beaverton.k12.or.us>
2016-11-03 14:31:28 -0700

    3rd Commit

--- StudentList.cpp
+++ StudentList.cpp
@@ -2,6 +2,7 @@
 #include <cstring>
 #include <vector>
 #include <iomanip>
+#include <cctype>
 
 using namespace std;
 
@@ -18,7 +19,7 @@ void print(vector<student*>* studentList);
 
 int main(){
   bool stillPlaying = true;
-  char input[30];  
+  char input[30];
   vector<student*> studentList;
 
   while (stillPlaying == true){
@@ -50,34 +51,40 @@ void add(vector<student*>* studentList){
   student* newStudent = new student;
   int newId;
   double newGPA;
-  char newFirstName;
-  char newLastName;
+  char newFirstName[250];
+  char newLastName[250];
+  
+  
+
   cout<<"Input ID:"<<endl;
   cin>>newId;
   cin.ignore();
-
+  
   cout<<"Input GPA:"<<endl;
   cin>>newGPA;
   cin.ignore();
-
+  
   cout<<"Input First Name:"<<endl;
-  cin>>newFirstName;
+  cin.get(newFirstName,250);
+  cin.ignore();
+  
+  cout<<"Input Last Name:"<<endl;
+  cin.get(newLastName,250);
   cin.ignore();
 
+  newStudent->lastName = newLastName;
+  newStudent->firstName = newFirstName;
   newStudent->GPA = newGPA;
   newStudent->id = newId;
-  newStudent->firstName = strdup(newFirstName);
-  newStudent->lastName = strdup(newLastName);
+  
 
-  student->push_back(newStudent);
-
+  studentList->push_back(newStudent);
   
 }
 void remove(vector<student*>* studentList){
   
+  
 }
 void print(vector<student*>* studentList){
-  for(int i = 0; i < studentList.size(); i++){
-   
-  }
+  
 }  
